9e53e82f45bfd5ed79741c2d073eea236078979f
ismail <ismailjosim99@gmail.com>
2023-10-19 20:16:13 +0600

    Add single brand cards

--- src/components/home/Brands.jsx
+++ src/components/home/Brands.jsx
@@ -1,17 +1,30 @@
+import { Link } from 'react-router-dom';
 import SectionHeading from '../../utils/SectionHeading';
 
 const heading = { primary: "Our Top Brands", secondary: "" };
 
 
 const Brands = () => {
-
+    // to={ `/brands/${ "item.name" }` }
 
     return (
         <section>
             <SectionHeading heading={ heading } />
-            <div className="w-11/12 mx-auto grid lg:grid-cols-6 md:grid-cols-3 grid-cols-2 gap-5">
+            <div className="w-11/12 mx-auto grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5">
+                {/* Single Brand */ }
+                { [1, 2, 3, 4, 5, 6].map((item, idx) => {
 
+                    return (
+                        <Link key={ idx } className="w-full text-center">
+                            <img className="object-cover object-center w-full h-60 mx-auto rounded-lg" src="https://images.unsplash.com/photo-1493863641943-9b68992a8d07?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=739&q=80" alt="avatar" />
+                        </Link>
+                    )
+                })
+
+                }
             </div>
+
+
         </section>
     );
 };
