6b61833874b7a62c89d786ef41d119aa1589442e
ismail <ismailjosim99@gmail.com>
2023-10-19 23:42:20 +0600

    single brand movies route added

--- src/components/home/Brands.jsx
+++ src/components/home/Brands.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import SectionHeading from '../../utils/SectionHeading';
 import { useEffect, useState } from 'react';
+import "../../styles/index.css"
 
 const heading = { primary: "Our Top Brands", secondary: "" };
 
@@ -13,7 +14,7 @@ const Brands = () => {
     useEffect(() => {
         setLoading(true);
         const fetchData = async () => {
-            const res = await fetch("https://moviedb-sigma-bice.vercel.app/brands");
+            const res = await fetch(`${ import.meta.env.VITE_SERVER_URL }/brands`);
             const data = await res.json();
             const allBrands = data.data;
             setLoading(false);
@@ -30,7 +31,7 @@ const Brands = () => {
                 {/* Single Brand */ }
                 { items && items.map((item, idx) => {
                     return (
-                        <Link to={ `/brands/${ item.brandName }` } key={ idx } className="w-full text-center">
+                        <Link to={ `/brands/${ item.brandName }` } key={ idx } className="w-full text-center brand_link">
                             <img className="w-full h-60 mx-auto rounded-lg border-2 p-5 hover:shadow-md transition-all ease-in-out duration-300 shadow-lg border-primary" src={ item.brandLogo } alt="avatar" />
                         </Link>
                     )
--- src/pages/SingleBrand.jsx
+++ src/pages/SingleBrand.jsx
@@ -1,13 +1,29 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 
 const SingleBrand = () => {
     const { name } = useParams();
-    console.log(name)
-    return (
-        <div>
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-        </div>
+
+    useEffect(() => {
+        setLoading(true);
+        const fetchData = async () => {
+            const res = await fetch(`${ import.meta.env.VITE_SERVER_URL }/movies/${ name }`);
+            const data = await res.json();
+            const allMovies = data.data;
+            setLoading(false);
+            setMovies(allMovies);
+        }
+        fetchData()
+    }, [])
+
+    return (
+        <section className='w-11/12 mx-auto'>
+
+        </section>
     );
 };
 
