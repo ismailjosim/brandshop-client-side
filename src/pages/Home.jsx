464174fda4da757eb44821ee5bbf5187f85628ef
ismail <ismailjosim99@gmail.com>
2023-10-19 20:31:51 +0600

    customer reviews added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../components/home/Hero';
 import Brands from '../components/home/Brands';
+import Reviews from '../components/home/Reviews';
 
 const Home = () => {
     return (
         <>
             <Hero />
             <Brands />
+            <Reviews />
         </>
     );
 };
